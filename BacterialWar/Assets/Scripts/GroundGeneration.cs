60e7cd532b5032041798343234aa4bc66ebbade2
Marko Savchuk <savchukmarko@gmail.com>
2020-05-07 13:22:14 +0300

    Delete Library folder

---
+++ BacterialWar/Assets/Scripts/GroundGeneration.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGeneration : MonoBehaviour
+{
+    //todo consider make it private with serialazible
+
+    public Transform HexPrefab;
+
+    public int 
+}
