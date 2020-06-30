8cca89f61b02306e18fa10a983a1ea6344aaaac2
Marko Savchuk <savchukmarko@gmail.com>
2020-06-30 18:02:28 +0300

    Update particles prefab, Create Particle collection

---
+++ BacterialWar/Assets/Scripts/Particles/ParticleCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollection : SingletonMonoBehaviour<ParticleCollection>
+{
+    #region Attacker
+
+    [SerializeField]
+    public GameObject DamageArea;
+
+    [SerializeField]
+    public GameObject DamageFreeze;
+
+    [SerializeField]
+    public GameObject DamageInfection;
+
+    [SerializeField]
+    public GameObject DamagePoint;
+
+    #endregion
+
+    #region Got Attacked
+
+    [SerializeField]
+    public GameObject Frozen;
+
+    [SerializeField]
+    public GameObject Infected;
+
+    #endregion
+}
