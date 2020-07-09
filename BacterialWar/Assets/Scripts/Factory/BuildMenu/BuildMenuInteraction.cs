5f12bda0cc0c383908db1ddcf3632b58d3877128
Marko Savchuk <savchukmarko@gmail.com>
2020-07-09 18:26:46 +0300

    Add virus images

--- BacterialWar/Assets/Scripts/Factory/BuildMenu/BuildMenuInteraction.cs
+++ BacterialWar/Assets/Scripts/Factory/BuildMenu/BuildMenuInteraction.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class BuildMenuInteraction : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _descriptionCanvas;
+
     public void CloseMenu()
-    {
+    {       
         FactoryBuilder.Instance.SelectedFactoryPosition = null;
         gameObject.SetActive(false);
+
+        _descriptionCanvas.SetActive(false);
+
+        FadeOutAllItems();
     }
 
     public void FadeOutAllItems()
@@ -16,7 +23,12 @@ public class BuildMenuInteraction : MonoBehaviour
         foreach (var item in gameObject.GetComponentsInChildren<FactoryMenuItemInteraction>(false))
         {
             var img = item.gameObject.GetComponent<Image>();
-            img.color = new Color(img.color.r, img.color.g, img.color.b, 0.4f);
+            img.color = new Color(img.color.r, img.color.g, img.color.b, 0.35f);
         }
+    }
+
+    public void OpenFactoryDescription(FactoryObject factoryObject)
+    {
+        _descriptionCanvas.SetActive(true);
     }
 }
--- BacterialWar/Assets/Scripts/Factory/BuildMenu/FactoryMenuItemInteraction.cs
+++ BacterialWar/Assets/Scripts/Factory/BuildMenu/FactoryMenuItemInteraction.cs
@@ -7,12 +7,22 @@ using UnityEngine.UI;
 
 public class FactoryMenuItemInteraction : MonoBehaviour
 {
+    private BuildMenuInteraction _buildMenuInteraction;
+
+    private void Awake()
+    {
+        _buildMenuInteraction = FactoryBuilder.Instance.BuildMenu.GetComponent<BuildMenuInteraction>();
+    }
+
     public void ChooseFactory(FactoryObject factoryObject)
     {
-        FactoryBuilder.Instance.BuildMenu.GetComponent<BuildMenuInteraction>().FadeOutAllItems();
+        //todo maybe refactor to call just one method
+        _buildMenuInteraction.FadeOutAllItems();
 
         var img = gameObject.GetComponent<Image>();
         img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
+
+        _buildMenuInteraction.OpenFactoryDescription(factoryObject);
     }
 
     public void BuildFactory(FactoryObject factoryObject)
